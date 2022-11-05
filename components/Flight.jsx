0df9cd29b73bb73a702bce2b9b04a52f0eaec749
Gonzalo Romero <akianzax@gmail.com>
2022-11-05 02:28:54 -0300

    Añadido para elegir fecha de ida y vuelta

--- components/Flight.jsx
+++ components/Flight.jsx
@@ -5,10 +5,16 @@ import FlightForm from "./FlightForm";
 
 const Flight = () => {
   const [flightInfo, setFlightInfo] = useState(null);
+  const [returnInfo, setReturnInfo] = useState(null);
+  const [isSearched, setIsSearched] = useState(false);
 
+  // Viaje de ida
   const from = useRef();
   const to = useRef();
   const date = useRef();
+
+  // Viaje de vuelta
+  const returnDate = useRef();
 
   const handleFlightInfo = (event) => {
     event.preventDefault();
@@ -19,6 +25,7 @@ const Flight = () => {
       from: from.current.value,
       to: to.current.value,
       date: date.current.value,
+      returnDate: returnDate.current?.value,
     };
     let flightExist = flights.filter((item) => {
       return (
@@ -27,28 +34,46 @@ const Flight = () => {
         data.date === item.data
       );
     });
-    flightExist.length ? setFlightInfo(flightExist) : setFlightInfo(null);
+    let returnExist = flights.filter((item) => {
+      return (
+        data.to === item.origin &&
+        data.from === item.destination &&
+        data.returnDate === item.data
+      );
+    });
+    returnExist.length ? setReturnInfo(returnExist) : setReturnInfo(null);
+    flightExist.length
+      ? setFlightInfo(flightExist)
+      : setFlightInfo(null) & setIsSearched(true);
   };
-
+console.log(returnInfo)
   return (
-    <>
+    <section className={styles.gridContainer}>
       <FlightForm
         from={from}
         to={to}
         date={date}
+        returnDate={returnDate}
         handleFlightInfo={handleFlightInfo}
       />
       {flightInfo ? (
         <div className={styles.detailsContainer}>
-          <p>Asientos disponibles: {flightInfo[0].availability}</p>
-          <p>Fecha: {flightInfo[0].data}</p>
-          <p>Origen: {flightInfo[0].origin}</p>
-          <p>Destino: {flightInfo[0].destination}</p>
+          <p>
+            Su vuelo sale el día {flightInfo[0].data} desde{" "}
+            {flightInfo[0].origin} hasta {flightInfo[0].destination}.
+            <br />
+            {returnInfo
+              ? `Con vuelta el día ${returnInfo[0].data} y un precio total
+            de ${(returnInfo[0].price + flightInfo[0].price).toFixed(2)} $`
+              : `Precio total: ${flightInfo[0].price} $`}
+          </p>
         </div>
+      ) : isSearched ? (
+        <p className={styles.notFlight}>No disponible en esta fecha</p>
       ) : (
-        <p className={styles.notFlight}>Sin viajes</p>
+        <p className={styles.notFlight}>Busca un viaje...</p>
       )}
-    </>
+    </section>
   );
 };
 
--- components/FlightForm.jsx
+++ components/FlightForm.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import styles from "../styles/FlightForm.module.css";
 
-const FlightForm = ({from, to, date, handleFlightInfo}) => {
+const FlightForm = ({ from, to, date, handleFlightInfo, returnDate }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  console.log(isChecked);
   return (
-    <form onSubmit={e => handleFlightInfo(e)} className={styles.container}>
+    <form onSubmit={(e) => handleFlightInfo(e)} className={styles.container}>
       <div className={styles.selectsContainer}>
-        <p>Desde:</p>
+        <p>Origen:</p>
         <select ref={from} className={styles.select} name="From">
           <option value="MDZ">Mendoza</option>
           <option value="COR">Córdoba</option>
@@ -14,19 +17,49 @@ const FlightForm = ({from, to, date, handleFlightInfo}) => {
       </div>
 
       <div className={styles.selectsContainer}>
-        <p>Hasta:</p>
+        <p>Destino:</p>
         <select ref={to} className={styles.select} name="To">
           <option value="MDZ">Mendoza</option>
           <option value="COR">Córdoba</option>
           <option value="EPA">Aeroparque, Buenos Aires</option>
-          <option value="BRC">San Caralos de Bariloche</option> 
+          <option value="BRC">San Caralos de Bariloche</option>
         </select>
       </div>
-    <div className={styles.selectsContainer}>
-        <p>Elija su fecha: </p>
-      <input ref={date} className={styles.select} type="date" name="Fecha" />
-    </div>
-    <button className={styles.search}>Buscar</button>
+      <div className={styles.selectsContainer}>
+        <div>
+
+        <label htmlFor="ida">Ida</label>
+        <input
+          onChange={() => setIsChecked(!isChecked)}
+          checked={!isChecked ? true : false}
+          type="radio"
+          name="ida"
+          id="ida"
+        />
+        <label htmlFor="vuelta">Ida y vuelta</label>
+        <input
+          checked={isChecked ? true : false}
+          onChange={() => setIsChecked(!isChecked)}
+          type="radio"
+          name="vuelta"
+          id="vuelta"
+          />
+        </div>
+        <p>Fecha de ida: </p>
+        <input ref={date} className={styles.select} type="date" name="Fecha" />
+        {isChecked ? (
+          <>
+            <p>Fecha de vuelta: </p>
+            <input
+              ref={returnDate}
+              className={styles.select}
+              type="date"
+              name="FechaVuelta"
+            />
+          </>
+        ) : undefined}
+      </div>
+      <button className={styles.search}>Buscar</button>
     </form>
   );
 };
